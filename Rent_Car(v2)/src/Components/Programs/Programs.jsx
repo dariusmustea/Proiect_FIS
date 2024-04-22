927e2869a24e130ac209ed980145cb88be70df4f
teodora.popescu <teodora.popescu@student.upt.ro>
2024-04-22 13:16:29 +0300

    1h

--- Rent_Car(v2)/src/Components/Programs/Programs.jsx
+++ Rent_Car(v2)/src/Components/Programs/Programs.jsx
@@ -8,7 +8,7 @@ const Programs = () => {
       <div className="program">
     <img src={program_1} alt="" />
     <div className="caption">
-      <p>Monaco, a small but mighty principality nestled on the French Riviera, is renowned as one of the world's
+      <p>A small, but mighty principality nestled on the French Riviera, is renowned as one of the world's
        most glamorous destinations. With its breathtaking views of the Mediterranean Sea, opulent casinos, and
         prestigious Grand Prix racing, Monaco embodies luxury and sophistication. </p>
     </div>
